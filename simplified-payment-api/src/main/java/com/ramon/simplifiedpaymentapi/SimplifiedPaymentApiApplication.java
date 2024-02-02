e09a0aeac14da49f6bfe1abdef431b88fba2708b
Ramon <ramonsilva12305@outlook.com>
2024-02-01 22:26:21 -0300

    feat/first commit

---
+++ simplified-payment-api/src/main/java/com/ramon/simplifiedpaymentapi/SimplifiedPaymentApiApplication.java
@@ -0,0 +1,13 @@
+package com.ramon.simplifiedpaymentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedPaymentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedPaymentApiApplication.class, args);
+	}
+
+}
---
+++ simplified-payment-api/src/test/java/com/ramon/simplifiedpaymentapi/SimplifiedPaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramon.simplifiedpaymentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedPaymentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
